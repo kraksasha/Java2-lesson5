27ce66ed3c4e3d4d7d931d6561bf04be4bbbdf62
kraksasha <kraksasha@yandex.ru>
2022-02-12 15:14:28 +0300

    Initial commit

---
+++ src/main/java/com/geekbrains/ThreadTest.java
@@ -0,0 +1,56 @@
+package com.geekbrains;
+
+public class ThreadTest {
+    static final int size = 10000000;
+    static final int h = size / 2;
+    static float[] arr = new float[size];
+
+    public static void main(String[] args) {
+        arrayTimeFilling();
+        arrayTimeFillingThread();
+    }
+
+    public static void arrayTimeFilling(){
+        for (int i = 0; i < size; i++){
+            arr[i] = i;
+        }
+        long a = System.currentTimeMillis();
+        for (int i = 0; i < size; i++){
+            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+        System.out.println(System.currentTimeMillis() - a);
+    }
+
+    public static void arrayTimeFillingThread(){
+        for (int i = 0; i < size; i++){
+            arr[i] = i;
+        }
+        long a = System.currentTimeMillis();
+        float a1[] = new float[h];
+        float a2[] = new float[h];
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < a1.length; i++){
+                    a1[i] = (float)(a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < a2.length; i++){
+                    a2[i] = (float)(a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+                }
+            }
+        }).start();
+
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+        System.out.println(System.currentTimeMillis() - a);
+    }
+}
